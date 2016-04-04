e506dc5e01836f6bfb52ca56c4ad51a8262b93b6
ollieh-m <ollie.hm@cantab.net>
2016-04-04 19:03:33 +0100

    Modified tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,32 +2,80 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-	it 'returns fizz when passed 3' do
-		expect(fizzbuzz(3)).to eq 'fizz'
-	end
+  let(:divs_of_3_not_5) do
+    arr = []
+    (1..30).each{ |i| arr.push(i) if ( i%3==0 and i%5!=0 ) }
+    arr
+  end
+  let(:divs_of_5_not_3) do
+    arr = []
+    (1..30).each{ |i| arr.push(i) if ( i%3!=0 and i%5==0 ) }
+    arr
+  end
+  let(:divs_of_5_and_3) do
+    arr = []
+    for i in (1..100) do
+      arr.push(i) if (i%3==0 and i%5==0)
+    end
+    arr
+  end
+  let(:div_not_of_5_or_3) do
+    arr = []
+    (1..30).each{ |i| arr.push(i) if ( i%3!=0 and i%5!=0 ) }
+    arr
+  end
 
-  it 'returns fizz when passed 9' do
+  context 'when passed multiples of 3 not 5' do
+    it 'returns fizz when given values between 1 and 30' do
+        divs_of_3_not_5.each do |multiples|
+          expect(fizzbuzz(multiples)).to eq 'fizz'
+        end
+    end  
+    it 'returns fizz when passed 3' do
+		  expect(fizzbuzz(3)).to eq 'fizz'
+    end
+    it 'returns fizz when passed 9' do
       expect(fizzbuzz(9)).to eq 'fizz'
+    end
   end
 
-	it 'returns buzz when passed 5' do
-		expect(fizzbuzz(5)).to eq 'buzz'
-	end
-  it 'returns buzz when passed 20' do
-    expect(fizzbuzz(20)).to eq 'buzz'
+	context 'when passed multiples of 5 not 3' do
+    it 'returns buzz when given values between 1 and 30' do
+        divs_of_5_not_3.each do |multiples|
+          expect(fizzbuzz(multiples)).to eq 'buzz'
+        end
+    end 
+    it 'returns buzz when passed 5' do
+		  expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    it 'returns buzz when passed 20' do
+      expect(fizzbuzz(20)).to eq 'buzz'
+    end
   end
 
+  context 'when passed multiples of 5 and 3' do
+    it 'returns fizzbuzz when given values between 1 and 100' do
+        divs_of_5_and_3.each do |multiples|
+          expect(fizzbuzz(multiples)).to eq 'fizzbuzz'
+        end
+    end
   	it 'returns fizzbuzz when passed 15' do
     	expect(fizzbuzz(15)).to eq 'fizzbuzz'
   	end
-
     it 'returns fizzbuzz when passed 45' do
       expect(fizzbuzz(45)).to eq 'fizzbuzz'
     end
+  end
 
-  	it 'returns 4 when passed 4' do
+  context 'when passed numbers not divisible by 3 or 5' do
+  	it 'returns the number' do
+      div_not_of_5_or_3.each do |number|
+        expect(fizzbuzz(number)).to eq number
+      end
+    end
+    it 'returns 4 when passed 4' do
   		expect(fizzbuzz(4)).to eq 4
   	end
-
+  end
 
 end
