a9b0b5fe3deb49b8670c7d58f8f28aafc7e32b98
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-04 14:13:58 +0100

    add test when passed 15 fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def fizzbuzz(num)
-  'fizz'
+  if num == 3
+    return 'fizz'
+  elsif num == 5
+    return "buzz"
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-	
+
 	it 'returns fizz when passed 3' do
 		expect(fizzbuzz(3)).to eq 'fizz'
 	end
@@ -9,5 +9,9 @@ describe 'fizzbuzz' do
 	it 'returns buzz when passed 5' do
 		expect(fizzbuzz(5)).to eq 'buzz'
 	end
-	
+
+  it 'return fizzbuzz when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
 end
